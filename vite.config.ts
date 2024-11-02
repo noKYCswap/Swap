bcfc87e46aed5e7d44e7cafab2df9b1ae1d86cc9
NoKYC <muddinb@proton.me>
2024-11-02 15:26:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/",
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:80', // Remplacez par l'URL de votre API
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    port: 3000, // Changez ce port selon vos besoins
   },
 });
